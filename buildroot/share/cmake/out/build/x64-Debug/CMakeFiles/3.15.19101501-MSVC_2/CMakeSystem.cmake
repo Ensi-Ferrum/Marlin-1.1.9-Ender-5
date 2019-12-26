fafe0f7fd1a236957c17d030ff854f23547e89c4
Ensi Ferrum <ensi.ferrum@outlook.de>
2019-12-26 09:25:27 +0100

    2019-12-26

---
+++ buildroot/share/cmake/out/build/x64-Debug/CMakeFiles/3.15.19101501-MSVC_2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-10.0.19536")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.19536")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+
+include("D:/3D Druck/Ender 5/Marlin FW/Marlin-1.1.x/Marlin-1.1.9-Ender-5/buildroot/share/cmake/marlin-cmake/toolchain/ArduinoToolchain.cmake")
+
+set(CMAKE_SYSTEM "Arduino")
+set(CMAKE_SYSTEM_NAME "Arduino")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
